0a9b8cc1455cdf2639516d06f2288a1953a1ced4
Marc Baloup <marc.baloup@laposte.net>
2018-03-27 22:04:33 +0200

    Préparation Battle dev

---
+++ 2017-2018/battle-dev-2018/ex2/com/isograd/exercise/IsoContest.java
@@ -0,0 +1,28 @@
+package com.isograd.exercise;
+
+import java.util.Scanner;
+
+public class IsoContest {
+
+	/*
+	 * Exercice 2
+	 */
+	public static void main(String[] args) {
+		try (Scanner s = new Scanner(System.in)) {
+			
+			// dans le cas ou la taille de l'entrée n'est pas indiqué comme première valeur de l'entrée
+			while (s.hasNext()) {
+				
+			}
+			
+			
+			// dans le cas où on a la taille de l'entrée en première valeur
+			int n = s.nextInt();
+			for (int i = 0; i < n; i++) {
+				
+			}
+			
+		}
+	}
+	
+}
