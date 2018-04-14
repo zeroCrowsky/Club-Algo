c4dc9f336692de36ed1e059255009f0c32edb2e8
Marc Baloup <marc.baloup@laposte.net>
2018-04-14 05:52:45 +0200

    Codejam 2018 Round 1A

---
+++ 2017-2018/codejam-2018/round-1-A/p1_waffle_choppers/Solution.java
@@ -0,0 +1,129 @@
+package p1_waffle_choppers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		try (Scanner s = new Scanner(System.in)) {
+			int t = s.nextInt();
+			for (int i = 0; i < t; i++) {
+				int R = s.nextInt();
+				int C = s.nextInt();
+				int h = s.nextInt();
+				int v = s.nextInt();
+				
+				boolean[][] mat = new boolean[R][C];
+				int[] rows = new int[R];
+				int[] cols = new int[C];
+				int total = 0;
+				Map<Integer, Integer> rowsDistribution = new HashMap<>();
+				Map<Integer, Integer> colsDistribution = new HashMap<>();
+				int[][] chocoDistribution = new int[h+1][v+1];
+				
+				for (int r = 0; r < R; r++) {
+					char[] line = s.next().toCharArray();
+					for (int c = 0; c < C; c++) {
+						boolean choco = line[c] == '@';
+						mat[r][c] = choco;
+						if (choco) {
+							rows[r]++;
+							cols[c]++;
+							total++;
+						}
+					}
+				}
+				
+				boolean impossible = false;
+				
+				if (total % (h + 1) != 0 || total % (v + 1) != 0) {
+					System.out.println("Case #" + (i + 1) + ": IMPOSSIBLE");
+					continue;
+				}
+				
+				// test rows
+				
+				int chocoPerCuttedRow = total / (h + 1);
+				
+				int currSum = 0;
+				int currRow = 0;
+				for (int r = 0; r < R; r++) {
+					currSum += rows[r];
+					rowsDistribution.put(r, currRow);
+					if (currSum > chocoPerCuttedRow) {
+						impossible = true;
+						break;
+					}
+					if (currSum == chocoPerCuttedRow) {
+						currSum = 0;
+						currRow++;
+					}
+				}
+				if (currSum != 0)
+					impossible = true;
+				
+				if (impossible) {
+					System.out.println("Case #" + (i + 1) + ": IMPOSSIBLE");
+					continue;
+				}
+				
+				// test cols
+				
+				int chocoPerCuttedCols = total / (v + 1);
+				
+				currSum = 0;
+				int currCol = 0;
+				for (int c = 0; c < C; c++) {
+					currSum += cols[c];
+					colsDistribution.put(c, currCol);
+					if (currSum > chocoPerCuttedCols) {
+						impossible = true;
+						break;
+					}
+					if (currSum == chocoPerCuttedCols) {
+						currSum = 0;
+						currCol++;
+					}
+				}
+				if (currSum != 0)
+					impossible = true;
+				
+				if (impossible) {
+					System.out.println("Case #" + (i + 1) + ": IMPOSSIBLE");
+					continue;
+				}
+
+				for (int r = 0; r < R; r++) {
+					for (int c = 0; c < C; c++) {
+						if (mat[r][c]) {
+							chocoDistribution[rowsDistribution.get(r)][colsDistribution.get(c)]++;
+						}
+					}
+				}
+				
+				int value = chocoDistribution[0][0];
+				for (int r = 0; r < h + 1 && !impossible; r++) {
+					for (int c = 0; c < v + 1; c++) {
+						if (chocoDistribution[r][c] != value) {
+							impossible = true;
+							break;
+						}
+					}
+				}
+				
+				if (impossible) {
+					System.out.println("Case #" + (i + 1) + ": IMPOSSIBLE");
+				}
+				else {
+					System.out.println("Case #" + (i + 1) + ": POSSIBLE");
+				}
+				
+				
+				
+			}
+		}
+	}
+	
+}
---
+++ 2017-2018/codejam-2018/round-1-A/p3_edgy_baking/Solution.java
@@ -0,0 +1,140 @@
+package p3_edgy_baking;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+ * Not working !!! (only correct for examples)
+ */
+public class Solution {
+	
+	public static void main(String[] args) {
+		try (Scanner s = new Scanner(System.in)) {
+			int T = s.nextInt();
+			for (int t = 0; t < T; t++) {
+				
+				int N = s.nextInt();
+				int P = s.nextInt();
+
+				List<Cookie> cookies = new ArrayList<>(N);
+				List<Boolean> dontCut = new ArrayList<>(N);
+				
+				for (int n = 0; n < N; n++) {
+					cookies.add(new Cookie(s.nextInt(), s.nextInt()));
+					dontCut.add(false);
+				}
+				
+				double currentP = recursive(cookies, dontCut, P);
+				
+				System.out.println("Case #" + (t + 1) + ": " + currentP);
+				
+				
+				
+				
+			}
+		}
+	}
+	
+	// return best P
+	static double recursive(List<Cookie> cookies, List<Boolean> dontCut, int P) {
+		double currentP = currentPerimeter(cookies);
+		
+		boolean lastPerfect = cleanCuttable(cookies, dontCut, currentP, P);
+		if (lastPerfect) {
+			return P;
+		}
+
+		double sumMinAdded = sumMinAdded(cookies, dontCut);
+		double sumMaxAdded = sumMaxAdded(cookies, dontCut);
+		
+		if (sumMaxAdded + currentP <= P)
+			return sumMaxAdded + currentP;
+		else if (sumMaxAdded + currentP > P && sumMinAdded + currentP <= P)
+			return P;
+		else {
+			double bestP = currentP;
+			for (int i = 0; i < cookies.size(); i++) {
+				if (dontCut.get(i))
+					continue;
+				dontCut.set(i, true);
+				double newP = recursive(cookies, dontCut, P);
+				dontCut.set(i, false);
+				if (newP > bestP)
+					bestP = newP;
+			}
+			
+			return bestP;
+		}
+	}
+	
+	/*
+	 * @return : si le dernier de la liste permet d'avoir exactement P
+	 */
+	static boolean cleanCuttable(List<Cookie> cuttables, List<Boolean> dontCut, double currentP, int P) {
+		cuttables.sort((c1, c2) -> Double.compare(c1.minAdded, c2.minAdded));
+		
+		for (int i = cuttables.size() - 1; i >= 0; i--) {
+			if (dontCut.get(i))
+				continue;
+			Cookie c = cuttables.get(i);
+			if (c.minAdded + currentP > P) {
+				dontCut.set(i, true);
+				continue;
+			}
+			if (c.minAdded + currentP <= P && c.maxAdded + currentP >= P) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	
+	static double currentPerimeter(List<Cookie> cookies) {
+		double sum = 0;
+		for (Cookie c : cookies) {
+			sum += c.perimeter;
+		}
+		return sum;
+	}
+
+	
+	static double sumMinAdded(List<Cookie> cookies, List<Boolean> dontCut) {
+		double sum = 0;
+		for (int i = 0; i < cookies.size(); i++) {
+			if (dontCut.get(i))
+				continue;
+			sum += cookies.get(i).minAdded;
+		}
+		return sum;
+	}
+	
+	static double sumMaxAdded(List<Cookie> cookies, List<Boolean> dontCut) {
+		double sum = 0;
+		for (int i = 0; i < cookies.size(); i++) {
+			if (dontCut.get(i))
+				continue;
+			sum += cookies.get(i).maxAdded;
+		}
+		return sum;
+	}
+	
+	
+	
+	
+	private static class Cookie {
+		int W, H;
+		double perimeter, minAdded, maxAdded;
+		
+		public Cookie(int w, int h) {
+			W = w;
+			H = h;
+			perimeter = (W + H) * 2;
+			minAdded = 2 * Math.min(W, H);
+			maxAdded = 2 * Math.sqrt(W*W + H*H);
+		}
+		
+	}
+	
+}
