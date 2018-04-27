93666561f439628f35e2966352879f31dfb80a3d
Marc Baloup <marc.baloup@etudiant.univ-lille1.fr>
2018-04-27 14:12:18 +0200

    Préparation catalyst 2018

---
+++ 2017-2018/coding-contest/src/Input.java
@@ -0,0 +1,25 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Input {
+	
+	// public data from input
+	
+	
+	
+	
+	public Input(InputStream in) {
+		@SuppressWarnings("resource")
+		Scanner s = new Scanner(in);
+		
+		// read input here
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ 2017-2018/coding-contest/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Input in = new Input(System.in);
+		
+		// compute and print here
+		
+		
+	}
+	
+	
+	
+	
+	
+}
